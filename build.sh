ae137c77a7e5e8882622ddaf827d692e82be75a5
Auston Stewart <auston@shedbuilt.net>
2018-05-30 08:36:32 -1000

    Fixing build script

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
             --docdir="$SHED_PKG_LOCAL_DOCDIR" &&
 # Build and Install
 make -j $SHED_NUM_JOBS &&
-make DESTDIR="$SHED_FAKE_ROOT" install
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Prune Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
